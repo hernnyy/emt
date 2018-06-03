415d6f388766effb22a71d2400f58ca37ec49595
hernnyy <jonhnick15@gmail.com>
2018-06-03 16:19:12 -0300

    calendar api y firebase DB api

--- app/src/main/java/com/example/hernan/esmiturno/LoginActivity.java
+++ app/src/main/java/com/example/hernan/esmiturno/LoginActivity.java
@@ -74,13 +74,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
@@ -342,7 +335,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
 
             String url = "http://ikaroira.com/ws-user.php/login";
             StringRequest strRequest = new StringRequest(Request.Method.POST, url,
@@ -394,21 +386,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             };
             NetManager.getInstance(mctx).addToRequestQueue(strRequest);
 
-//            try {
-//                // Simulate network access.
-//                Thread.sleep(6000);
-//            } catch (InterruptedException e) {
-//                return false;
-//            }
-//            for (String credential : DUMMY_CREDENTIALS) {
-//                String[] pieces = credential.split(":");
-//                if (pieces[0].equals(mEmail)) {
-//                    // Account exists, return true if the password matches.
-//                    return pieces[1].equals(mPassword);
-//                }
-//            }
-
-            // TODO: register the new account here.
             return true;
         }
 
